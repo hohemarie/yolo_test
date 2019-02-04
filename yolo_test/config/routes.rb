7e990c28fa0382d32375cae8bc17968d8a73e57c
marie goudon <marie.goudon@laposte.net>
2019-02-04 11:40:36 +0100

    ajout des fichiers

--- yolo_test/config/routes.rb
+++ yolo_test/config/routes.rb
@@ -1,7 +1,7 @@
 root 'controller#index'
 Rails.application.routes.draw do 
-  get 'message/show' 
+  get 'message/:user_entry', to: 'message#show'
   get 'static_pages/home'
-  get 'static_pages/contact', to: 'static_pages#contact'
+  get 'static_pages/contact', to: 'controller#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
