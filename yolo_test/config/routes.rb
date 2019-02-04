6bf3fa5a00e5400c83b4df45ca006fb06a66519a
Marie <marie.goudon@laposte.net>
2019-02-04 10:25:02 +0100

    ajout dun controller

--- yolo_test/config/routes.rb
+++ yolo_test/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'message/show'
   get 'static_pages/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
